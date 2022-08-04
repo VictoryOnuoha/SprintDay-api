8974eb630156778a7a5ad57c1c197a86fc278523
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-04 18:40:24 -0400

    Created routes for projects

---
+++ routes/projects.js
@@ -0,0 +1,22 @@
+//Initialize server & routing
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    res.send('get all projects');
+});
+
+
+router.post('/', (req, res) => {
+
+    res.send('create new project');
+})
+
+// update a project
+router.put('/:projectid');
+
+// delete a project
+router.delete('/:projectid');
+
+module.exports = router;
