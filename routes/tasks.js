edbac0d5c5af148656e369d4624ec7d042b0730c
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-04 18:56:59 -0400

    Created routing for tasks

---
+++ routes/tasks.js
@@ -0,0 +1,21 @@
+//Initialize server & routing
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    res.send('get all tasks');
+});
+
+router.post('/', (req, res) => {
+
+    res.send('create new tasks');
+});
+
+//update tasks
+router.put('/taskid');
+
+// delete a project
+router.delete('/:taskid');
+
+module.exports = router;
