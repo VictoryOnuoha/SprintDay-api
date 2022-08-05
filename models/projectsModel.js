56ba50c4f525d2f35a577ac197d25efcf5ec645a
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-05 13:26:49 -0400

    Created data logic and modified data structure for projects

--- models/projectsModel.js
+++ models/projectsModel.js
@@ -1,6 +1,29 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+// Read project data 
 const readProjects = () => {
-    const jsondata = fs.readFileSync()
+    const projectsDataFile = fs.readFileSync('./data/projects.json', 'utf-8');
+    const projectsData = JSON.parse(projectsDataFile);
+    return projectsData;
+}
+
+//write project data back to file
+const writeProjects = (projectsData) => {
+    fs.writeFileSync('./data/projects.json', JSON.stringify(projectsData));
+}
+
+// get projects
+const getProjects = () => {
+    return readProjects();
+}
+
+// get project with specific id
+const getProjectFiltered = () => {
+    return readProjects().map(project => {
+        return {
+            id: project.id,
+            
+        }
+    })
 }
